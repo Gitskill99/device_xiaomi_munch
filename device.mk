8cd0627d0bfc5c5905a45b4a2edb7ad04635406c
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:35:01 +0200

    munch: Nuke OTA Preopt Script
    - OTA Dexopt optimizations taking a lot of time, let's skip it on installation moment

--- device.mk
+++ device.mk
@@ -73,12 +73,6 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
@@ -90,8 +84,7 @@ PRODUCT_PACKAGES += \
     update_verifier
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+    checkpoint_gc
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
